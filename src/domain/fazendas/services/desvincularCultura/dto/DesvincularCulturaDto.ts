c00add8eeba4124a004b806837e75565996b1416
erykscarabellodias <erykscarabello97@gmail.com>
2024-01-22 13:38:55 -0300

    refactor: apagando arquivo não usado

--- src/domain/fazendas/services/desvincularCultura/dto/DesvincularCulturaDto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsUUID } from "class-validator";
-
-export default class DesvincularCulturaDto {
-  @IsNotEmpty({ message: "O campo idCultura é obrigatório" })
-  @IsUUID("4", { message: "O campo idCultura deve ser um UUID" })
-  idCultura: string;
-}
