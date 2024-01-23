f52066da1c3955b30bab273aea9db6fb91dfec78
erykscarabellodias <erykscarabello97@gmail.com>
2024-01-22 22:22:04 -0300

    fix: injetando busca de cep na edição de fazendas

--- src/http/controllers/fazendas/editarFazenda/index.ts
+++ src/http/controllers/fazendas/editarFazenda/index.ts
@@ -1,15 +1,18 @@
 import FazendaRepository from "../../../../domain/fazendas/repositories/FazendaRepository";
 import EditarFazendaService from "../../../../domain/fazendas/services/editarFazenda/EditarFazendaService";
+import BuscarCepServiceViaCep from "../../../../shared/services/buscarCep/BuscarCepViaCepService";
 import ValidarUuidService from "../../../../shared/services/validarUuid/ValidarUuidService";
 import CriarFazendaController from "./EditarFazendaController";
 
 const criarFazenda = (): CriarFazendaController => {
   const fazendaRepository = new FazendaRepository();
   const validarUuidService = new ValidarUuidService();
+  const buscarCepService = new BuscarCepServiceViaCep();
 
   const service = new EditarFazendaService(
     fazendaRepository,
-    validarUuidService
+    validarUuidService,
+    buscarCepService
   );
 
   const controller = new CriarFazendaController(service);
